95c0d33bb44cddff81a31cb51caa64c19e7157d3
Emmanuel Yakubu <tethcode@gmail.com>
2025-05-17 15:03:51 +0100

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -77,12 +77,11 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
 
 
 # Password validation
